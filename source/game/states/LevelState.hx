b04ba35568feb57486a92b71f781499e373116ca
Kino  Rose <kinoarose@gmail.com>
2021-02-28 21:42:07 -0500

    Fix player movement/add player movement

--- source/game/states/LevelState.hx
+++ source/game/states/LevelState.hx
@@ -9,9 +9,18 @@ import flixel.addons.editors.tiled.TiledObjectLayer;
 import flixel.addons.editors.tiled.TiledMap;
 
 class LevelState extends BaseTileState {
+	public var player:Player;
+
 	override public function createLevelInfo() {
 		var tileLayer:TiledTileLayer = cast(map.getLayer('Level'));
+		player = new Player(60, 60, cast DepotData.Actors_Kikiyo);
+
 		createLevelMap(tileLayer);
+	}
+
+	override public function addGroups() {
+		super.addGroups();
+		add(player);
 	}
 
 	override public function createUI() {}
