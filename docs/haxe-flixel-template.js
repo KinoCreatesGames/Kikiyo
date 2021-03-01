51ef8b78e8d51c19c9b47242c262ada17e9638ee
kinoar <kinarose@gmail.com>
2021-03-01 01:58:09 +0000

    Update game website

--- docs/haxe-flixel-template.js
+++ docs/haxe-flixel-template.js
@@ -869,7 +869,7 @@ ApplicationMain.main = function() {
 ApplicationMain.create = function(config) {
 	var app = new openfl_display_Application();
 	ManifestResources.init(config);
-	app.meta.h["build"] = "10";
+	app.meta.h["build"] = "11";
 	app.meta.h["company"] = "KinoCreatesGames";
 	app.meta.h["file"] = "haxe-flixel-template";
 	app.meta.h["name"] = "Kikiyo";
@@ -4043,7 +4043,7 @@ openfl_display_Sprite.prototype = $extend(openfl_display_DisplayObjectContainer.
 });
 var Main = function() {
 	openfl_display_Sprite.call(this);
-	this.addChild(new flixel_FlxGame(0,0,game_states_LevelState));
+	this.addChild(new flixel_FlxGame(0,0,game_states_LevelOneState));
 };
 $hxClasses["Main"] = Main;
 Main.__name__ = "Main";
@@ -4062,9 +4062,9 @@ DocumentClass.__super__ = Main;
 DocumentClass.prototype = $extend(Main.prototype,{
 	__class__: DocumentClass
 });
-var DepotData = function() { };
-$hxClasses["DepotData"] = DepotData;
-DepotData.__name__ = "DepotData";
+var AssetPaths = function() { };
+$hxClasses["AssetPaths"] = AssetPaths;
+AssetPaths.__name__ = "AssetPaths";
 var EReg = function(r,opt) {
 	this.r = new RegExp(r,opt.split("u").join(""));
 };
@@ -4247,7 +4247,7 @@ ManifestResources.init = function(config) {
 	openfl_text_Font.registerFont(_$_$ASSET_$_$OPENFL_$_$flixel_$fonts_$nokiafc22_$ttf);
 	openfl_text_Font.registerFont(_$_$ASSET_$_$OPENFL_$_$flixel_$fonts_$monsterrat_$ttf);
 	var bundle;
-	var data = "{\"name\":null,\"assets\":\"aoy4:pathy42:assets%2Fmaps%2FTilemap-data-goes-here.txty4:sizezy4:typey4:TEXTy2:idR1y7:preloadtgoR2i3672R3y5:SOUNDR5y32:assets%2Fsounds%2Fmouse-over.wavy9:pathGroupaR8hR6tgoR2i6900R3R7R5y31:assets%2Fsounds%2Fmenu-open.wavR9aR10hR6tgoR2i4584R3R7R5y33:assets%2Fsounds%2Fbullet-fire.wavR9aR11hR6tgoR2i8410R3R7R5y28:assets%2Fsounds%2Fimpact.wavR9aR12hR6tgoR2i19472R3R7R5y31:assets%2Fsounds%2Fpause-out.wavR9aR13hR6tgoR2i8378R3R7R5y34:assets%2Fsounds%2Fbutton-click.wavR9aR14hR6tgoR2i4578R3R7R5y31:assets%2Fsounds%2Fmenu-exit.wavR9aR15hR6tgoR2i12492R3R7R5y34:assets%2Fsounds%2Fenemy-impact.wavR9aR16hR6tgoR2i22964R3R7R5y30:assets%2Fsounds%2Fpause-in.wavR9aR17hR6tgoR0y36:assets%2Fsounds%2Fsounds-go-here.txtR2zR3R4R5R18R6tgoR2i39740R3R7R5y32:assets%2Fsounds%2Flow-health.wavR9aR19hR6tgoR0y29:assets%2Fimages%2Fcog-two.pngR2i512R3y5:IMAGER5R20R6tgoR0y28:assets%2Fimages%2Fbutton.pngR2i1126R3R21R5R22R6tgoR0y31:assets%2Fimages%2Fexit-door.pngR2i382R3R21R5R23R6tgoR0y36:assets%2Fimages%2Fimages-go-here.txtR2zR3R4R5R24R6tgoR0y37:assets%2Fimages%2Faffection-heart.pngR2i685R3R21R5R25R6tgoR0y34:assets%2Fimages%2Fdialog-arrow.pngR2i261R3R21R5R26R6tgoR0y36:assets%2Fmusic%2Fmusic-goes-here.txtR2zR3R4R5R27R6tgoR0y28:assets%2Fdata%2Fdatabase.dpoR2i6893R3R4R5R28R6tgoR0y34:assets%2Fdata%2Fdata-goes-here.txtR2zR3R4R5R29R6tgoR2i39706R3y5:MUSICR5y28:flixel%2Fsounds%2Fflixel.mp3R9aR31y28:flixel%2Fsounds%2Fflixel.ogghR6tgoR2i2114R3R30R5y26:flixel%2Fsounds%2Fbeep.mp3R9aR33y26:flixel%2Fsounds%2Fbeep.ogghR6tgoR2i5794R3R7R5R34R9aR33R34hgoR2i33629R3R7R5R32R9aR31R32hgoR2i15744R3y4:FONTy9:classNamey35:__ASSET__flixel_fonts_nokiafc22_ttfR5y30:flixel%2Ffonts%2Fnokiafc22.ttfR6tgoR2i29724R3R35R36y36:__ASSET__flixel_fonts_monsterrat_ttfR5y31:flixel%2Ffonts%2Fmonsterrat.ttfR6tgoR0y33:flixel%2Fimages%2Fui%2Fbutton.pngR2i519R3R21R5R41R6tgoR0y36:flixel%2Fimages%2Flogo%2Fdefault.pngR2i3280R3R21R5R42R6tgh\",\"rootPath\":null,\"version\":2,\"libraryArgs\":[],\"libraryType\":null}";
+	var data = "{\"name\":null,\"assets\":\"aoy4:pathy42:assets%2Fmaps%2FTilemap-data-goes-here.txty4:sizezy4:typey4:TEXTy2:idR1y7:preloadtgoR0y27:assets%2Fmaps%2Fkikiyo.ldtkR2i52324R3R4R5R7R6tgoR0y45:assets%2Fmaps%2Fkikiyo%2Ftiled%2Fkikiyo.worldR2i76R3R4R5R8R6tgoR0y42:assets%2Fmaps%2Fkikiyo%2Ftiled%2FLevel.tmxR2i2006R3R4R5R9R6tgoR2i3672R3y5:SOUNDR5y32:assets%2Fsounds%2Fmouse-over.wavy9:pathGroupaR11hR6tgoR2i6900R3R10R5y31:assets%2Fsounds%2Fmenu-open.wavR12aR13hR6tgoR2i4584R3R10R5y33:assets%2Fsounds%2Fbullet-fire.wavR12aR14hR6tgoR2i8410R3R10R5y28:assets%2Fsounds%2Fimpact.wavR12aR15hR6tgoR2i19472R3R10R5y31:assets%2Fsounds%2Fpause-out.wavR12aR16hR6tgoR2i8378R3R10R5y34:assets%2Fsounds%2Fbutton-click.wavR12aR17hR6tgoR2i4578R3R10R5y31:assets%2Fsounds%2Fmenu-exit.wavR12aR18hR6tgoR2i12492R3R10R5y34:assets%2Fsounds%2Fenemy-impact.wavR12aR19hR6tgoR2i22964R3R10R5y30:assets%2Fsounds%2Fpause-in.wavR12aR20hR6tgoR0y36:assets%2Fsounds%2Fsounds-go-here.txtR2zR3R4R5R21R6tgoR2i39740R3R10R5y32:assets%2Fsounds%2Flow-health.wavR12aR22hR6tgoR0y35:assets%2Fimages%2Ffloor-tileset.pngR2i691R3y5:IMAGER5R23R6tgoR0y29:assets%2Fimages%2Fcog-two.pngR2i512R3R24R5R25R6tgoR0y28:assets%2Fimages%2Fbutton.pngR2i1126R3R24R5R26R6tgoR0y31:assets%2Fimages%2Fexit-door.pngR2i382R3R24R5R27R6tgoR0y36:assets%2Fimages%2Fimages-go-here.txtR2zR3R4R5R28R6tgoR0y37:assets%2Fimages%2Faffection-heart.pngR2i685R3R24R5R29R6tgoR0y34:assets%2Fimages%2Fdialog-arrow.pngR2i261R3R24R5R30R6tgoR0y36:assets%2Fmusic%2Fmusic-goes-here.txtR2zR3R4R5R31R6tgoR0y28:assets%2Fdata%2Fdatabase.dpoR2i10457R3R4R5R32R6tgoR0y34:assets%2Fdata%2Fdata-goes-here.txtR2zR3R4R5R33R6tgoR2i39706R3y5:MUSICR5y28:flixel%2Fsounds%2Fflixel.mp3R12aR35y28:flixel%2Fsounds%2Fflixel.ogghR6tgoR2i2114R3R34R5y26:flixel%2Fsounds%2Fbeep.mp3R12aR37y26:flixel%2Fsounds%2Fbeep.ogghR6tgoR2i5794R3R10R5R38R12aR37R38hgoR2i33629R3R10R5R36R12aR35R36hgoR2i15744R3y4:FONTy9:classNamey35:__ASSET__flixel_fonts_nokiafc22_ttfR5y30:flixel%2Ffonts%2Fnokiafc22.ttfR6tgoR2i29724R3R39R40y36:__ASSET__flixel_fonts_monsterrat_ttfR5y31:flixel%2Ffonts%2Fmonsterrat.ttfR6tgoR0y33:flixel%2Fimages%2Fui%2Fbutton.pngR2i519R3R24R5R45R6tgoR0y36:flixel%2Fimages%2Flogo%2Fdefault.pngR2i3280R3R24R5R46R6tgh\",\"rootPath\":null,\"version\":2,\"libraryArgs\":[],\"libraryType\":null}";
 	var manifest = lime_utils_AssetManifest.parse(data,ManifestResources.rootPath);
 	var library = lime_utils_AssetLibrary.fromManifest(manifest);
 	lime_utils_Assets.registerLibrary("default",library);
@@ -48248,7 +48248,7 @@ game_states_BaseTileState.prototype = $extend(flixel_FlxState.prototype,{
 		this.add(this.enemyGrp);
 	}
 	,createLevelMap: function(tileLayer) {
-		var tileset = this.map.getTileSet("Tileset");
+		var tileset = this.map.getTileSet("Floor_tileset");
 		if(tileLayer == null) {
 			tileLayer = null;
 		} else {
@@ -48257,7 +48257,7 @@ game_states_BaseTileState.prototype = $extend(flixel_FlxState.prototype,{
 		this.createDecorationLayers();
 	}
 	,createDecorationLayers: function() {
-		var tileset = this.map.getTileSet("Tileset");
+		var tileset = this.map.getTileSet("Floor_tileset");
 		var tileLayer = this.map.getLayer("Decoration");
 		if(tileLayer != null) {
 			var levelDecoration = new flixel_tile_FlxTilemap();
@@ -48292,11 +48292,33 @@ game_states_LevelState.__name__ = "game.states.LevelState";
 game_states_LevelState.__super__ = game_states_BaseTileState;
 game_states_LevelState.prototype = $extend(game_states_BaseTileState.prototype,{
 	createLevelInfo: function() {
-		haxe_Log.trace(DepotData.Actors.lines,{ fileName : "source/game/states/LevelState.hx", lineNumber : 5, className : "game.states.LevelState", methodName : "createLevelInfo"});
+		var tileLayer = this.map.getLayer("Level");
+		this.createLevelMap(tileLayer);
 	}
 	,createUI: function() {
 	}
+	,processCollision: function() {
+		game_states_BaseTileState.prototype.processCollision.call(this);
+	}
+	,processLevel: function(elapsed) {
+	}
+	,tilesetPath: function() {
+		return "assets/images/floor-tileset.png";
+	}
 	,__class__: game_states_LevelState
+});
+var game_states_LevelOneState = function(MaxSize) {
+	game_states_LevelState.call(this,MaxSize);
+};
+$hxClasses["game.states.LevelOneState"] = game_states_LevelOneState;
+game_states_LevelOneState.__name__ = "game.states.LevelOneState";
+game_states_LevelOneState.__super__ = game_states_LevelState;
+game_states_LevelOneState.prototype = $extend(game_states_LevelState.prototype,{
+	create: function() {
+		game_states_LevelState.prototype.create.call(this);
+		this.createLevel("assets/maps/kikiyo/tiled/Level.tmx");
+	}
+	,__class__: game_states_LevelOneState
 });
 var haxe_StackItem = $hxEnums["haxe.StackItem"] = { __ename__ : "haxe.StackItem", __constructs__ : ["CFunction","Module","FilePos","Method","LocalFunction"]
 	,CFunction: {_hx_index:0,__enum__:"haxe.StackItem",toString:$estr}
@@ -66812,7 +66834,7 @@ var lime_utils_AssetCache = function() {
 	this.audio = new haxe_ds_StringMap();
 	this.font = new haxe_ds_StringMap();
 	this.image = new haxe_ds_StringMap();
-	this.version = 753764;
+	this.version = 822941;
 };
 $hxClasses["lime.utils.AssetCache"] = lime_utils_AssetCache;
 lime_utils_AssetCache.__name__ = "lime.utils.AssetCache";
@@ -108257,13 +108279,31 @@ openfl_display_DisplayObject.__tempStack = new lime_utils_ObjectPool(function() 
 },function(stack) {
 	stack.set_length(0);
 });
-DepotData.Actors_Kikiyo = { spd : 100, name : "Kikiyo", guid : "a5cbeb88-acd3-46aa-a15b-0a9d849078f3", id : "0", atk : 1, def : 0};
-DepotData.Actors_ActorName = { spd : 100, name : "ActorName", guid : "817661b6-a5fb-434f-8b5f-57b72e87c913", id : "1", atk : 2, def : 0};
-DepotData.Actors = { name : "actors", lines : [{ spd : 100, name : "Kikiyo", guid : "a5cbeb88-acd3-46aa-a15b-0a9d849078f3", id : "0", atk : 1, def : 0},{ spd : 100, name : "ActorName", guid : "817661b6-a5fb-434f-8b5f-57b72e87c913", id : "1", atk : 2, def : 0}], isProps : false, hidden : false, guid : "9ee981be-907f-4a41-9014-cbac2ab9b9a4", displayColumn : "id", description : "List of actors and enemies in the game.", configurable : { name : "text", description : "text", displayColumn : "columnSelect@name", "columnSelect@name@displayColumn" : { allowedTypes : ["int","float","text","longtext"]}}};
-DepotData.Switches_Met_Mentor = { name : "Met Mentor", guid : "94fc7cdb-c962-4df6-a48e-470088c50894", id : "0"};
-DepotData.Switches = { name : "Switches", lines : [{ name : "Met Mentor", guid : "94fc7cdb-c962-4df6-a48e-470088c50894", id : "0"}], isProps : false, hidden : false, guid : "585e7418-2ed3-4d65-a2d8-1944a6d4707b", displayColumn : "id", description : "A sheet used to define the number of switches in game", configurable : { name : "text", description : "text", displayColumn : "columnSelect@name", "columnSelect@name@displayColumn" : { allowedTypes : ["int","float","text","longtext"]}}};
-DepotData.Variables_Met_Mentor_Count = { name : "Met Mentor Count", guid : "cd45808c-7ca1-471f-b1fc-af82cc72e71a", id : "0"};
-DepotData.Variables = { name : "Variables", lines : [{ name : "Met Mentor Count", guid : "cd45808c-7ca1-471f-b1fc-af82cc72e71a", id : "0"}], isProps : false, hidden : false, guid : "90b1a923-6d73-42fc-a53a-67cc3267aaf6", displayColumn : "id", description : "A sheet used to track the progress of variables within the game.", configurable : { name : "text", description : "text", displayColumn : "columnSelect@name", "columnSelect@name@displayColumn" : { allowedTypes : ["int","float","text","longtext"]}}};
+AssetPaths.Tilemap_data_goes_here__txt = "assets/maps/Tilemap-data-goes-here.txt";
+AssetPaths.kikiyo__ldtk = "assets/maps/kikiyo.ldtk";
+AssetPaths.kikiyo__world = "assets/maps/kikiyo/tiled/kikiyo.world";
+AssetPaths.Level__tmx = "assets/maps/kikiyo/tiled/Level.tmx";
+AssetPaths.mouse_over__wav = "assets/sounds/mouse-over.wav";
+AssetPaths.menu_open__wav = "assets/sounds/menu-open.wav";
+AssetPaths.bullet_fire__wav = "assets/sounds/bullet-fire.wav";
+AssetPaths.impact__wav = "assets/sounds/impact.wav";
+AssetPaths.pause_out__wav = "assets/sounds/pause-out.wav";
+AssetPaths.button_click__wav = "assets/sounds/button-click.wav";
+AssetPaths.menu_exit__wav = "assets/sounds/menu-exit.wav";
+AssetPaths.enemy_impact__wav = "assets/sounds/enemy-impact.wav";
+AssetPaths.pause_in__wav = "assets/sounds/pause-in.wav";
+AssetPaths.sounds_go_here__txt = "assets/sounds/sounds-go-here.txt";
+AssetPaths.low_health__wav = "assets/sounds/low-health.wav";
+AssetPaths.floor_tileset__png = "assets/images/floor-tileset.png";
+AssetPaths.cog_two__png = "assets/images/cog-two.png";
+AssetPaths.button__png = "assets/images/button.png";
+AssetPaths.exit_door__png = "assets/images/exit-door.png";
+AssetPaths.images_go_here__txt = "assets/images/images-go-here.txt";
+AssetPaths.affection_heart__png = "assets/images/affection-heart.png";
+AssetPaths.dialog_arrow__png = "assets/images/dialog-arrow.png";
+AssetPaths.music_goes_here__txt = "assets/music/music-goes-here.txt";
+AssetPaths.database__dpo = "assets/data/database.dpo";
+AssetPaths.data_goes_here__txt = "assets/data/data-goes-here.txt";
 Globals.GAME_TITLE = "Love Defense ";
 Globals.GAME_SAVE_SLOTS = 5;
 Globals.TEXT_START = "Start";
@@ -109497,7 +109537,7 @@ game_ext_KColor.DARK_BYZANTIUM = -11392185;
 game_ext_KColor.MORNING_BLUE = -7492963;
 game_ext_KColor.PRETTY_PINK = -37993;
 game_ext_KColor.LIGHT_ORANGE = -28314;
-game_states_BaseTileState.TILESET_NAME = "Tileset";
+game_states_BaseTileState.TILESET_NAME = "Floor_tileset";
 haxe_Int32._mul = Math.imul != null ? Math.imul : function(a,b) {
 	return a * (b & 65535) + (a * (b >>> 16) << 16 | 0) | 0;
 };
